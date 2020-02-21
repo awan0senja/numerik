571efd297b7c079d2a043d0a2e155c21999a2bec
awan0senja <60491423+awan0senja@users.noreply.github.com>
2020-02-21 17:28:41 +0700

    Update grafik_subplots.py

--- python_script/grafik_subplots.py
+++ python_script/grafik_subplots.py
@@ -23,3 +23,5 @@ ax[1,1].set_title("Kurva Polinom Empat")
 
 p.suptitle("Latihan Kurva keempat")
 p.show()
+
+#Sumber: https://towardsdatascience.com/matplotlib-tutorial-learn-basics-of-pythons-powerful-plotting-library-b5d1b8f67596
